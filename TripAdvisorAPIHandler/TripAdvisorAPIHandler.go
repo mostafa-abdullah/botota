b95e354b30b51bf75706d25c33a7dba8e8bdbda9
Hagar Yasser <hagar.y.omar@gmail.com>
2016-11-10 00:22:26 +0200

    TripAdvisor funcs signatures

---
+++ TripAdvisorAPIHandler/TripAdvisorAPIHandler.go
@@ -0,0 +1,22 @@
+package TripAdvisorAPIHandler
+
+//getHotels receives the destination TripAdvisor ID &
+//returns an array of IDs of the top 10 hotels.
+func getHotels(destinationID string) []string{
+
+}
+//getNearRestaurants receives the chosen hotel TripAdvisor ID &
+//returns an arrays of IDs of the top 10 nearby restaurants.
+func getNearRestaurants(hotelID string) []string{
+
+}
+//getAttractions receives the destination TripAdvisor ID &
+//returns an array of IDs of the top 10 attractions.
+func getAttractions(destinationID string) []string{
+
+}
+//createSchedule receives the TripAdvisor IDs of the destination,the chosen hotel, start date & end date &
+//returns a schedule with timing allocated for each attraction and for lunch time.
+func createSchedule(destinationID string, hotelID string, startDate string, endDate string) string{
+
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+//createSchedule receives the TripAdvisor IDs of the destination,the chosen hotel, start date & end date &
+//returns a schedule with timing allocated for each attraction and for lunch time
+//with travelling distance into consideration using Google Maps
+func createSchedule(destinationID string, hotelID string, startDate string, endDate string) string{
+
+}
