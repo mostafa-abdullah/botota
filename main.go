def1663c73793484f4e07cdd67962adf5c22a716
Hagar Yasser <hagar.y.omar@gmail.com>
2016-11-14 13:56:06 +0200

    added return statements, compiles correctly now

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-//createSchedule receives the TripAdvisor IDs of the destination,the chosen hotel, start date & end date &
+func main(){
+
+}
+
+
+//createSchedule receives the TripAdvisor IDs of the destination,the chosen hotel, start date & end date
 //returns a schedule with timing allocated for each attraction and for lunch time
 //with travelling distance into consideration using Google Maps
 func createSchedule(destinationID string, hotelID string, startDate string, endDate string) string{
 
+  return "";
 }
