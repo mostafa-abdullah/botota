5f396d07210d0e77fdaac4bf79cca968a9488af8
Mostafa Abdullah <t-moabd@microsoft.com>
2016-11-16 16:38:30 +0200

    Created initial server structure

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
+import (
+  "botota/server"
+)
+
 func main(){
-
+  server.StartServer()
 }
-
 
 //createSchedule receives the TripAdvisor IDs of the destination,the chosen hotel, start date & end date
 //returns a schedule with timing allocated for each attraction and for lunch time
