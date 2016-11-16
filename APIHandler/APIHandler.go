41027fea380fb21ff691898c5817c9b93c6db6ba
Hagar Yasser <hagar.y.omar@gmail.com>
2016-11-16 17:21:09 +0200

    merged conflicts

--- APIHandler/APIHandler.go
+++ APIHandler/APIHandler.go
@@ -1,12 +1,28 @@
 package APIHandler
 import (
-    "fmt"
     "net/http"
     "github.com/satori/go.uuid"
+    "botota/models"
+    "botota/database"
 )
 func WelcomeHandler(w http.ResponseWriter, r *http.Request){
+  //create user uuid
   u := createUserUUID();
-  
+
+  //create a new User model
+  user := models.User{Uuid: u}
+
+  //insert the new user to the database
+  database.CreateUser(user);
+
+  //get first question
+  q := database.GetFirstQuestion().Text;
+
+  //create response
+  res := {"message": q, "uuid": u}
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(res);
+
 }
 func createUserUUID() string {
   u := uuid.NewV4()
