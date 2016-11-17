c3127b7b3a15d24ba813d4849c162a6d56c8931d
Hagar Yasser <hagar.y.omar@gmail.com>
2016-11-17 21:46:39 +0200

    finalized GetHotels - all GoogleAPIHandler ready

--- GooglePlacesAPIHandler/GooglePlacesAPIHandler.go
+++ GooglePlacesAPIHandler/GooglePlacesAPIHandler.go
@@ -1,53 +1,57 @@
 package GooglePlacesAPIHandler
 
 import (
-  "sort"
-  "googlemaps.github.io/maps"
-  "golang.org/x/net/context"
-  "botota/utils"
-  "github.com/kr/pretty"
+	"botota/utils"
+	"github.com/kr/pretty"
+	"golang.org/x/net/context"
+	"googlemaps.github.io/maps"
+	"sort"
 )
-const(
-  MaxRestDist = 2000
+
+const (
+	MaxRestDist = 2000
 )
-//getHotels receives the destination TripAdvisor ID &
-//returns an array of IDs of the top 10 hotels.
-func getHotels(destinationID string) []string{
-  hotels := []string{}
-  return hotels
+
+//GetHotels receives the destination city &
+//returns an array of Places for the top 20 hotels.
+func GetHotels(destination string) []Place {
+	r := textSearch("hotels", destination)
+	h := formatResults(r)
+	sort.Sort(ByRating(h))
+	pretty.Println(h)
+	return h
 
 }
-//getNearRestaurants receives the chosen hotel TripAdvisor ID &
-//returns an arrays of IDs of the top 10 nearby restaurants.
-func GetNearRestaurants(hotel Place) []Place{
-  client := CreateClient()
-  r := &maps.NearbySearchRequest{
-    Location:   &hotel.Location,
-    Type:       "restaurant",
-    Radius:     MaxRestDist }
-  resp, err := client.NearbySearch(context.Background(), r)
-  utils.Check(err)
-  rest := formatResults(resp.Results)
-  sort.Sort(ByRating(rest))
-  pretty.Println(rest)
 
-  return rest
+//GetNearRestaurants receives the chosen hotel as a Place struct
+//returns an arrays of Places for the top 20 nearby restaurants.
+func GetNearRestaurants(hotel Place) []Place {
+	client := CreateClient()
+	r := &maps.NearbySearchRequest{
+		Location: &hotel.Location,
+		Type:     "restaurant",
+		Radius:   MaxRestDist}
+	resp, err := client.NearbySearch(context.Background(), r)
+	utils.Check(err)
+	rest := formatResults(resp.Results)
+	sort.Sort(ByRating(rest))
+	pretty.Println(rest)
+
+	return rest
 }
 
-
-
-
-//getAttractions receives the destination &
-//returns an array of IDs of the top 20 attractions.
-func GetAttractions(destination string) []Place{
-  r := textSearch("attractions", destination)
-  a  := formatResults(r)
-  sort.Sort(ByRating(a))
-  pretty.Println(a)
-  return a
+//GetAttractions receives the destination &
+//returns an array of Places for the top 20 attractions.
+func GetAttractions(destination string) []Place {
+	r := textSearch("attractions", destination)
+	a := formatResults(r)
+	sort.Sort(ByRating(a))
+	pretty.Println(a)
+	return a
 }
+
 //createSchedule receives the TripAdvisor IDs of the destination,the chosen hotel, start date & end date &
 //returns a schedule with timing allocated for each attraction and for lunch time.
-func createSchedule(destinationID string, hotelID string, startDate string, endDate string) string{
-  return "";
+func createSchedule(destinationID string, hotelID string, startDate string, endDate string) string {
+	return ""
 }
--- GooglePlacesAPIHandler/helperFuncs.go
+++ GooglePlacesAPIHandler/helperFuncs.go
@@ -1,50 +1,54 @@
 package GooglePlacesAPIHandler
+
 import (
-  "googlemaps.github.io/maps"
-  "golang.org/x/net/context"
-  "botota/utils"
+	"botota/utils"
+	"golang.org/x/net/context"
+	"googlemaps.github.io/maps"
 )
-type Place struct{
-  Name string
-  Location maps.LatLng
-  Rating float32
+
+type Place struct {
+	Name     string
+	Location maps.LatLng
+	Rating   float32
 }
+
 const (
-  APIKey = "AIzaSyCNRXCIOJkenWGvhiIgu58ncqL6W9VOc3Y"
+	APIKey = "AIzaSyCNRXCIOJkenWGvhiIgu58ncqL6W9VOc3Y"
 )
 
 // ByRating implements sort.Interface for []maps.PlacesSearchResult based on
 // the Rating field.
 type ByRating []Place
+
 func (a ByRating) Len() int           { return len(a) }
 func (a ByRating) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByRating) Less(i, j int) bool { return a[i].Rating > a[j].Rating }
 
-func CreateClient() *maps.Client{
-  c, err := maps.NewClient(maps.WithAPIKey(APIKey))
-  utils.Check(err);
-  return c;
+func CreateClient() *maps.Client {
+	c, err := maps.NewClient(maps.WithAPIKey(APIKey))
+	utils.Check(err)
+	return c
 }
 
-func formatResults(results []maps.PlacesSearchResult) []Place{
-  places := []Place{}
-  for _,r := range results {
-    p := Place{r.Name, r.Geometry.Location, r.Rating}
-    places = append(places, p)
-  }
-  return places
+func formatResults(results []maps.PlacesSearchResult) []Place {
+	places := []Place{}
+	for _, r := range results {
+		p := Place{r.Name, r.Geometry.Location, r.Rating}
+		places = append(places, p)
+	}
+	return places
 }
 
 func textSearch(placeType string, destination string) []maps.PlacesSearchResult {
-  client := CreateClient()
+	client := CreateClient()
 
-  q := placeType + " in " + destination
+	q := placeType + " in " + destination
 
-  r := &maps.TextSearchRequest{Query: q}
+	r := &maps.TextSearchRequest{Query: q}
 
-  resp, err := client.TextSearch(context.Background(), r)
-  utils.Check(err)
+	resp, err := client.TextSearch(context.Background(), r)
+	utils.Check(err)
 
-  results := resp.Results
-  return results
+	results := resp.Results
+	return results
 }
