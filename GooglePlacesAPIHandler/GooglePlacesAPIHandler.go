21aee0fc15e2555d54c57b73f1912a8a00cf7128
Hagar Yasser <hagar.y.omar@gmail.com>
2016-11-17 21:25:34 +0200

    finalized GetAttractions

--- GooglePlacesAPIHandler/GooglePlacesAPIHandler.go
+++ GooglePlacesAPIHandler/GooglePlacesAPIHandler.go
@@ -1,7 +1,6 @@
 package GooglePlacesAPIHandler
 
 import (
-  "github.com/kr/pretty"
   "sort"
 )
 
@@ -24,15 +23,12 @@ func getNearRestaurants(hotelID string) []string{
 
 //getAttractions receives the destination &
 //returns an array of IDs of the top 20 attractions.
-func GetAttractions(destination string) []string{
-  results := textSearch("attractions", destination)
-  places  := formatResults(results)
-  sort.Sort(ByRating(places))
-
-	pretty.Println(places)
-
-  attractions := []string{}
-  return attractions
+func GetAttractions(destination string) []Place{
+  r := textSearch("attractions", destination)
+  h  := formatResults(r)
+  sort.Sort(ByRating(h))
+  //pretty.Println(h)
+  return h
 }
 //createSchedule receives the TripAdvisor IDs of the destination,the chosen hotel, start date & end date &
 //returns a schedule with timing allocated for each attraction and for lunch time.
